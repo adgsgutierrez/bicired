0ccc5d03954caddb3c25e1ee178f9214b00da7a7
Aric Dayan Gutierrez Sanchez <aricdayangutierrezsanchez@MacBook-Pro-de-Aric.local>
2018-08-14 19:51:08 -0500

    cambio de login y splash

--- app/src/main/java/co/com/konrad/bicired/SplashActivity.java
+++ app/src/main/java/co/com/konrad/bicired/SplashActivity.java
@@ -5,41 +5,31 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.widget.VideoView;
 import co.com.konrad.bicired.co.com.konrad.bicired.utils.Constants;
 
 public class SplashActivity extends Activity {
 
-    private VideoView videoSplash;
+    private static int SPLASH_TIME_OUT = 3000;
 
-   @Override
+    @Override
    protected void onCreate(Bundle sa){
        super.onCreate(sa);
-       try{
-           setContentView(R.layout.activity_splash);
-           videoSplash = (VideoView) findViewById(R.id.videoView);
-           Uri path = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video);
-           videoSplash.setVideoURI(path);
-           videoSplash.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-               public void onCompletion(MediaPlayer mp) {
-                   endSplash();
-               }
-           });
-           videoSplash.start();
-       }catch(Exception e){
-           Log.e(Constants.TAG_ERROR, "Error al iniciar el splash "+e.getMessage());
-           endSplash();
-       }
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+                endSplash();
+           }
+       }, SPLASH_TIME_OUT);
    }
 
    private void endSplash(){
-
-       if(isFinishing()){
-            return;
-        }
-
         startActivity(new Intent(this , StartActivity.class));
         overridePendingTransition(R.anim.zoom_forward_in, R.anim.zoom_forward_out);
+        finish();
    }
 }
