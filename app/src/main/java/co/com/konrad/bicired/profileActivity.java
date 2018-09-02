57a6e94c3203432615f65a9259321956bcdb8b7c
BrayanBaronUK <36496494+BrayanBaronUK@users.noreply.github.com>
2018-09-02 12:54:26 -0500

    button implementation to find location on the map
    button implementation to find location on the map

--- app/src/main/java/co/com/konrad/bicired/profileActivity.java
+++ app/src/main/java/co/com/konrad/bicired/profileActivity.java
@@ -3,6 +3,7 @@ package co.com.konrad.bicired;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.Button;
 public class profileActivity extends AppCompatActivity {
 
     Button volverInicio;
+    Button BuscarUbicacion;
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -19,7 +21,10 @@ public class profileActivity extends AppCompatActivity {
         super.onCreate(BackStart);
         setContentView(R.layout.activity_home);
 
+
         volverInicio = findViewById(R.id.btn1);
+        BuscarUbicacion = findViewById(R.id.btn2);
+
 
         volverInicio.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,11 +33,18 @@ public class profileActivity extends AppCompatActivity {
                 Intent volverInicio = new Intent(profileActivity.this, StartActivity.class);
                 startActivity(volverInicio);
             }
+        }
+        );
+        BuscarUbicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent BuscarUbicacion = new Intent(Intent.ACTION_VIEW);
+                BuscarUbicacion.setData(Uri.parse("geo:0,0?z=4&q;=parques"));
+                startActivity(BuscarUbicacion);
+            }
         });
-
-
     }
-
 
 }
 
