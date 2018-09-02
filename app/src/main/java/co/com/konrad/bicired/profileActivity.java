ef5396b5c91834377e3473b48709f04a1c2957ff
BrayanBaronUK <36496494+BrayanBaronUK@users.noreply.github.com>
2018-09-02 00:48:14 -0500

    implementation button return to home profile to home
    implementation button return to home profile to home

--- app/src/main/java/co/com/konrad/bicired/profileActivity.java
+++ app/src/main/java/co/com/konrad/bicired/profileActivity.java
@@ -1,12 +1,40 @@
 package co.com.konrad.bicired;
 
-import android.content.Intent
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
 
-package co.com.konrad.bicired.view;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class profileActivity {
+public class profileActivity extends AppCompatActivity {
+
+    Button volverInicio;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate (Bundle BackStart){
+
+        super.onCreate(BackStart);
+        setContentView(R.layout.activity_home);
+
+        volverInicio = findViewById(R.id.btn1);
+
+        volverInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent volverInicio = new Intent(profileActivity.this, StartActivity.class);
+                startActivity(volverInicio);
+            }
+        });
+
+
+    }
+
+
 }
+
+
+
