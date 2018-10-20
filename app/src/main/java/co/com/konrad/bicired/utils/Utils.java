41045eace669c7a38daa5ada2e902867ecad7c54
Aric Dayan Gutierrez Sanchez <adgsgutierrez@gmail.com>
2018-10-20 08:13:33 -0500

    Ajustes * Mejora del PopUp de Mensajes * Ajuste de Icono * Eliminacion del icono de Configuracion

--- app/src/main/java/co/com/konrad/bicired/utils/Utils.java
+++ app/src/main/java/co/com/konrad/bicired/utils/Utils.java
@@ -1,25 +1,46 @@
 package co.com.konrad.bicired.utils;
 
+import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
 
 import co.com.konrad.bicired.R;
 
 public class Utils {
 
-    public static void mostrarAlerta (Context context , String mensaje){
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        //Adicionando Mensaje
-        builder.setMessage(mensaje);
-        // Add the button
-        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener(){
-            public void onClick(DialogInterface dialog, int id) {
-                // User clicked OK button
+
+
+    public static void mostrarAlerta (Context context , String mensaje ){
+        Dialog customDialog = null;
+        // con este tema personalizado evitamos los bordes por defecto
+        customDialog = new Dialog(context,R.style.Theme_Dialog_Translucent);
+        //deshabilitamos el título por defecto
+        customDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //obligamos al usuario a pulsar los botones para cerrarlo
+        customDialog.setCancelable(false);
+        //establecemos el contenido de nuestro dialog
+        customDialog.setContentView(R.layout.dialogo_mensajes);
+
+        TextView titulo = (TextView) customDialog.findViewById(R.id.titulo);
+        titulo.setText("Ups!!!");
+
+        TextView contenido = (TextView) customDialog.findViewById(R.id.contenido);
+        contenido.setText(mensaje);
+
+        final Dialog customFinal = customDialog;
+
+        ((Button) customDialog.findViewById(R.id.aceptar)).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view)
+            {
+                customFinal.dismiss();
             }
         });
-        // Create the AlertDialog
-        AlertDialog dialog = builder.create();
-        dialog.show();
+
+        customDialog.show();
     }
 }
